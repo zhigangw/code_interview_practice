6a19750b728a46e51dbac20a7bf8abb3db6d7ba0
Zhigang Wang <zhigangw@hotmail.com>
2024-03-27 16:26:35 -0700

    minor

--- InterviewPractice.cpp
+++ InterviewPractice.cpp
@@ -9,6 +9,6 @@ int main()
 {
 	int a[] = { 8, 9, 10, 11, 0, 1, 2, 3, 4, 5, 6, 7 };
 	int index = find_in_rotated_array(a, sizeof(a) / sizeof(int), 2);
-	cout << "Hello CMake:"<< index << endl;
+	cout << "Hello CMake: "<< index << endl;
 	return 0;
 }
