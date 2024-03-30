92eee808c7ce4b1590f024f74a64f1aaae8c60ab
Zhigang Wang <zhigangw@hotmail.com>
2024-03-30 13:02:58 -0700

    max_subarray

---
+++ max_subarray.cpp
@@ -0,0 +1,18 @@
+int max_subarray(int* a, int n) {
+	// scan the array, and check if the current value + accumulated value > current value
+	// if yes, accumulated value = current value + accumulated value 
+	// if no, accumulated value = current value 
+	// use a max_sum to remeber the max value of accumulated one
+	int accumulated = 0;
+	int max_sum = 0;
+	for (int i = 0; i < n; i++) {
+		if (accumulated + a[i] > accumulated) {
+			accumulated = accumulated + a[i];
+		}
+		else {
+			accumulated = a[i];
+		}
+		if (max_sum < accumulated) max_sum = accumulated;
+	}
+	return max_sum;
+}
