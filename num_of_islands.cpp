a9cd3970b01643ead7b339e2c84929309041a8d5
Zhigang Wang <zhigangw@hotmail.com>
2024-04-01 19:08:15 -0700

    num of islands

--- num_of_islands.cpp
+++ num_of_islands.cpp
@@ -1,30 +1,31 @@
+#include <iostream>
 #include <vector>
 using namespace std;
 
-class Solution {
-public:
-    int numIslands(vector<vector<char>>& grid) {
-        int m = grid.size(), n = m ? grid[0].size() : 0, islands = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == '1') {
-                    islands++;
-                    eraseIslands(grid, i, j);
-                }
+/*
+* for each node, if it is 1, use dfs to travers all the ajacent points(1), change the 1 to 0 to avoid re-visit.
+* count the number of connected components
+*/
+void erase_lands(vector<vector<int>>& map, int i, int j) {
+    if (i < 0 || i >= map.size() || j < 0 || j >= map.size()) return;
+    if (map[i][j] == 0) return;
+    map[i][j] = 0;
+    erase_lands(map, i - 1, j); // left
+    erase_lands(map, i, j - 1); // up
+    erase_lands(map, i + 1, j); // right
+    erase_lands(map, i, j + 1); // down
+}
+
+int num_of_islands(vector<vector<int>>& map) {
+    int num_of_islands = 0;
+    for (auto i = 0; i < map.size(); i++) {
+        for (auto j = 0; j < map.size(); j++)
+        {
+            if (map[i][j]) {
+                num_of_islands++;
+                erase_lands(map, i, j);
             }
         }
-        return islands;
     }
-private:
-    void eraseIslands(vector<vector<char>>& grid, int i, int j) {
-        int m = grid.size(), n = grid[0].size();
-        if (i < 0 || i == m || j < 0 || j == n || grid[i][j] == '0') {
-            return;
-        }
-        grid[i][j] = '0';
-        eraseIslands(grid, i - 1, j);
-        eraseIslands(grid, i + 1, j);
-        eraseIslands(grid, i, j - 1);
-        eraseIslands(grid, i, j + 1);
-    }
-};
+    return num_of_islands;
+}
