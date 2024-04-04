aa748a9390f83a18fbfea30facae720844cbe2cb
Zhigang Wang <zhigangw@hotmail.com>
2024-04-04 10:17:32 -0700

    house_robber

--- house_robber.cpp
+++ house_robber.cpp
@@ -2,7 +2,7 @@
 /* the order is: prev2, prev1, num  */
 #include <vector>
 using namespace std;
-int rob(vector<int> nums) {
+int rob_dp(vector<int> nums) {
     if (nums.size() == 0) return 0;
     int prev1 = 0;
     int prev2 = 0;
@@ -12,4 +12,14 @@ int rob(vector<int> nums) {
         prev2 = tmp;
     }
     return prev1;
+}
+
+int rob(vector<int>& nums) {
+    return rob_rs(nums, nums.size() - 1);
+}
+int rob_rs(vector<int>& nums, int i) {
+    if (i < 0) {
+        return 0;
+    }
+    return max(rob_rs(nums, i - 2) + nums[i], rob_rs(nums, i - 1));
 }
